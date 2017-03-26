ecedfc55cb8d0f26cd7fe0084c82c6f7282da8ac
uhonogo <poliesternopo@gmail.com>
2017-03-26 20:13:30 +0300

    rsz

--- js/tabs.js
+++ js/tabs.js
@@ -1,12 +1,1 @@
-$(function () {
-    var tabContainers = $('div.tabs > div'); // получаем массив контейнеров
-    tabContainers.hide().filter(':first').show(); // прячем все, кроме первого
-    // далее обрабатывается клик по вкладке
-    $('div.tabs ul.tabNavigation a').click(function () {
-        tabContainers.hide(); // прячем все табы
-        tabContainers.filter(this.hash).show();
-        $('div.tabs ul.tabNavigation a').removeClass('selected');
-        $(this).addClass('selected');
-        return false;
-    }).filter(':first').click();
-});
+$(function(){var tabContainers=$('div.tabs > div');tabContainers.hide().filter(':first').show();$('div.tabs ul.tabNavigation a').click(function(){tabContainers.hide();tabContainers.filter(this.hash).show();$('div.tabs ul.tabNavigation a').removeClass('selected');$(this).addClass('selected');return false;}).filter(':first').click();});
